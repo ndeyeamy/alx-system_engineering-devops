350224e1041d00da69bb5ff884640946db93aaf8
ndeyeamy <falamy2006@gmail.com>
2023-12-18 05:04:48 -0800

    tout

--- 0x0C-web_server/nginx_manual_conf.sh
+++ 0x0C-web_server/nginx_manual_conf.sh
@@ -6,7 +6,8 @@ sudo touch /etc/nginx/mime.types /etc/nginx/nginx.conf
 sudo mkdir -p /etc/nginx/sites-available/ /etc/nginx/sites-enabled
 sudo touch /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
-# solving missing /run/nginx.pid
+# solving missing /run/nginx.pi
+
 sudo mkdir -p /etc/systemd/system/nginx.service.d
 sudo echo "[Service]\nExecStartPost=/bin/sleep 0.1\n" > /etc/systemd/system/nginx.service.d/override.conf
 sudo systemctl daemon-reload
