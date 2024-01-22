5a913dfca2ab516975d852d36caeac91ec860eeb
ndeyeamy <falamy2006@gmail.com>
2024-01-22 07:49:57 -0800

    t

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
-"""Request employee ID from API
-"""
-
-from json import load
-import requests
-from sys import argv
+""" Gather data from an API  """
 
 if __name__ == "__main__":
+    from requests import get
+    from sys import argv, exit
 
-    def make_request(resource, param=None):
-        """Retrieve user from API
-        """
-        url = 'https://jsonplaceholder.typicode.com/'
-        url += resource
-        if param:
-            url += ('?' + param[0] + '=' + param[1])
+    try:
+        id = argv[1]
+        is_int = int(id)
+    except:
+        exit()
 
-        # make request
-        r = requests.get(url)
+    url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
+    url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
 
-        # extract json response
-        r = r.json()
-        return r
+    r_user = get(url_user)
+    r_todo = get(url_todo)
 
-    user = make_request('users', ('id', argv[1]))
-    tasks = make_request('todos', ('userId', argv[1]))
-    tasks_completed = [task for task in tasks if task['completed']]
+    try:
+        js_user = r_user.json()
+        js_todo = r_todo.json()
 
-    print('Employee {} is done with tasks({}/{}):'.format(user[0]['name'],
-                                                          len(tasks_completed),
-                                                          len(tasks)))
-    for task in tasks_completed:
-        print('\t {}'.format(task['title']))
+    except ValueError:
+        print("Not a valid JSON")
+
+    if js_user and js_todo:
+        EMPLOYEE_NAME = js_user[0].get('name')
+        TOTAL_NUMBER_OF_TASKS = len(js_todo)
+        NUMBER_OF_DONE_TASKS = sum(item.get("completed")
+                                   for item in js_todo if item)
+
+        print("Employee {} is done with tasks({}/{}):"
+              .format(EMPLOYEE_NAME,
+                      NUMBER_OF_DONE_TASKS,
+                      TOTAL_NUMBER_OF_TASKS))
+        for todo in js_todo:
+            TASK_TITLE = todo.get('title')
+            if todo.get("completed"):
+                print("\t {}".format(TASK_TITLE))
